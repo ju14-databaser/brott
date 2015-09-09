254f910a13c6caa91bd16dfe43ffb18b3fb24c54
hejenstedt <hejenstedt@users.noreply.github.com>
2015-09-09 19:10:33 +0200

    Commit genom svn i eclipse

--- src/main/java/controller/BrottController.java
+++ src/main/java/controller/BrottController.java
@@ -3,7 +3,8 @@ package controller;
 public class BrottController {
 	
 	public static void main(String[] args) {
-		System.out.println("Anna");
+		System.out.println("Testar att göra commit med svn från eclipse");
+	
 	}
 
 }
