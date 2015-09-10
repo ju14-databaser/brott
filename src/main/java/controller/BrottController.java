2e367efe4fe9bed5b856c5aab31fa7ec2cd78127
hejenstedt <hejenstedt@users.noreply.github.com>
2015-09-10 14:01:31 +0200

    så programmet ska funka?

--- src/main/java/controller/BrottController.java
+++ src/main/java/controller/BrottController.java
@@ -1,17 +1,19 @@
 package controller;
 
+import org.springframework.ui.Model;
+
 import model.Crime;
 import model.CrimeHandler;
 
 
 public class BrottController {
 	
-	public String getCrime(Model model){
-		CrimeHandler crimeHandler = new CrimeHandler();
-		
-		Crime crime = crimeHandler.getCrimeFromPolice();
-		model.addAttribute("crime",crime);
-		return "home";
-	}
+//	public String getCrime(Model model){
+//		CrimeHandler crimeHandler = new CrimeHandler();
+//		
+//		Crime crime = crimeHandler.getCrimeFromPolice();
+//		model.addAttribute("crime",crime);
+//		return "home";
+//	}
 
 }
